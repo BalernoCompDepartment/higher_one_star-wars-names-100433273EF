e71d64afcd5238b837eb148398114fccb4038d8c
100433273EF <100433273@ea.edin.sch.uk>
2021-01-13 14:44:15 +0000

    a lot

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner; 
 class Main {
+
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    String forename = Keyboard.getText("Please enter your first name.");
+    String surname = Keyboard.getText("Please enter your surname.");
+    String maiden = Keyboard.getText("Please enter your mother's maiden name.");
+    String city = Keyboard.getText("Please enter the city you were born in.");
+
+    String starWarsName = surname.substring(0,3) + forename.substring(0,2).toLowerCase() + " " + maiden.substring(0,3) + city.substring(0,2).toLowerCase();
+
+    System.out.println("Your Star Wars name is " + starWarsName);
+
+
+    
+    
   }
 }
